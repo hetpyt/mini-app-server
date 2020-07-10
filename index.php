27b344db3bfcfe71e675d535b3af62a09705b7ae
hetpyt <43849401+hetpyt@users.noreply.github.com>
2020-07-10 12:23:32 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,23 +7,6 @@ use Jacwright\RestServer\RestServer;
 spl_autoload_register(); // don't load our classes unless we use them
 global $_Config;
 
-class MyRestServer extends RestServer {
-    public function getPostData() {
-        return $this->data;
-    }
-
-    public function getData() {
-        $data = file_get_contents('php://input');
-        //print_r($data);
-        $data = json_decode($data, false);
-        //print_r($data);
-
-		return $data;
-	}
-
-}
-
-//$mode = 'debug'; // 'debug' or 'production'
 $mode = $_Config['server_mode'];
 $server = new RestServer($mode);
 if ($mode === 'debug') {
